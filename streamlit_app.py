3a48097dea458458d4a6289ea3c1fcfa5ef0d6a7
JawaharSamuvel8952 <130992603+JawaharSamuvel8952@users.noreply.github.com>
2023-04-17 18:56:46 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit
 
 # streamlit.title('My Parents New Healthy Diner')
-streamlit.title('My Mom"'"s New Healthy Diner')
+streamlit.title('My Mom\'s New Healthy Diner')
 
                 🥣 🥗 🐔 🥑🍞
 
