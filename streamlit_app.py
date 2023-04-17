5e97df950d591afe7a8a8bcc46b70fc38052f6a8
JawaharSamuvel8952 <130992603+JawaharSamuvel8952@users.noreply.github.com>
2023-04-17 18:57:15 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,8 +3,6 @@ import streamlit
 # streamlit.title('My Parents New Healthy Diner')
 streamlit.title('My Mom\'s New Healthy Diner')
 
-                🥣 🥗 🐔 🥑🍞
-
 # streamlit.header('Breakfast Menu')
 streamlit.header('Breakfast Favorites')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
