a7de4d31d60ec95f443fd20e7aded39c21b6f928
JawaharSamuvel8952 <130992603+JawaharSamuvel8952@users.noreply.github.com>
2023-04-20 18:38:33 +0530

    adding stop command

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,6 +39,9 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # write your own comment - what does this do?
 streamlit.dataframe(fruityvice_normalized)
 
+# Don't run anything past here while we troubleshoot
+streamlit.stop()
+
 #20-04-2023
 import snowflake.connector
 
