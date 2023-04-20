f93d792cde0e77e287628cdc7cfaa53e8df930ca
JawaharSamuvel8952 <130992603+JawaharSamuvel8952@users.noreply.github.com>
2023-04-20 18:16:30 +0530

    adding 2nd text entry box

--- streamlit_app.py
+++ streamlit_app.py
@@ -55,3 +55,9 @@ my_cur.execute("SELECT * from fruit_load_list")
 my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit load lost contains:")
 streamlit.dataframe(my_data_rows)
+
+
+# add a 2nd text entry box with jackfruit
+
+add_my_fruit = streamlit.text_input('What fruit would you like to add?','jackfruit')
+streamlit.write('Thanks for adding ', add_my_fruit)
